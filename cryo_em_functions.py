9af610a533d92f295db5005d61b390ec9c7ba807
Melissa <melissa@ori.co>
2024-02-09 16:35:17 +0000

    Messing around with project documentation

---
+++ cryo_em_functions.py
@@ -0,0 +1,29 @@
+class cryo_em
+"""A simple class for handling image data to facilitate the reconstruction process in cryo-EM."""
+
+    def cluster_images(image_folder, num_clusters): 
+        """
+        This function clusters the micrographs stored in the specified image_folder 
+        using a clustering algorithm, facilitating efficient organisation and analysis of the data. 
+        [TBC based on decision routine provided, may just be an image loader].
+        Parametres:
+        Input/Output/Return:
+        """
+        
+    def particle_picking(micrograph, template): 
+        """
+        Designed to enhance particle identification accuracy, 
+        this function employs template matching techniques to locate individual protein 
+        particles within a given micrograph. It compares the micrograph with a user-defined template, 
+        enabling precise particle picking while minimising false positives.
+        Parametres:
+        Input/Output/Return:
+        """
+        
+    def classify_particles(particles): 
+        """
+        This function categorises the detected particles based on their features, 
+        allowing for further analysis and processing of the cryo-EM data.
+        Parametres:
+        Input/Output/Return:
+        """
